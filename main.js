4caa570f0debdf5386268a683690f496089da90b
Antoine Drouault <antoinedrouault@hotmail.com>
2022-03-31 22:23:56 +0200

    Add filter to only have words with letters found

--- main.js
+++ main.js
@@ -3,26 +3,54 @@ const wordLengthButton = document.getElementById("word-length-button")
 const lettersSubmitButton = document.getElementById("letters-submit")
 const results = document.getElementById("results")
 const letters = document.getElementById("letters")
-const letterButtons = document.getElementsByClassName("letter-button")
 
 wordLengthButton.addEventListener("click", handleWordLengthSubmit)
-lettersSubmitButton.addEventListener("click", getAllResults)
+lettersSubmitButton.addEventListener("click", handleLettersSubmit)
+
+let lettersFound = []
 
 function handleWordLengthSubmit() {
   for (let i = 0; i < parseInt(wordLengthInput.value); i++) {
     const letterContainer = `<div class="letter-container">
 <input class="letter-input" type="text" maxlength="1" size="1" />
+<button class="letter-button">Found</button>
 </div>`
     letters.innerHTML = letters.innerHTML + letterContainer
   }
+  const letterButtons = document.getElementsByClassName("letter-button")
+  const letterInputs = document.getElementsByClassName("letter-input")
+  letterInputs[0].classList.add("found")
+  addEventListeners(letterButtons, "click", handleLetterButtonClick)
 }
 
-function addEventListeners(elements, event) {
+function addEventListeners(elements, eventType, eventHandler) {
   for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", event)
+    elements[i].addEventListener(eventType, eventHandler)
   }
 }
 
+function handleLetterButtonClick(e) {
+  e.preventDefault()
+  const buttons = document.getElementsByClassName("letter-button")
+  const letterInputToModify = document.getElementsByClassName("letter-input")[indexInClass(buttons, e.target)]
+  letterInputToModify.classList.toggle("found")
+}
+
+function handleLettersSubmit(e) {
+  handleLettersFound()
+  getAllResults(e)
+}
+
+function handleLettersFound() {
+  const letterInputs = document.getElementsByClassName("letter-input")
+  for (let i = 0; i < letterInputs.length; i++) {
+    if (/^[a-zA-Z]+$/.test(letterInputs[i].value) && letterInputs[i].classList.contains("found")) {
+      lettersFound.push(letterInputs[i].value)
+    } else {
+      lettersFound.push("?")
+    }
+  }
+}
 
 function getAllResults(e) {
   e.preventDefault()
@@ -45,17 +73,27 @@ function getAllResults(e) {
   }
 
   const uniqueWordsArray = wordsArray.filter(onlyUnique)
-  
-  for (let i = 0; i < parseInt(uniqueWordsArray.length); i++) {
+
+  const wordsOnlyWithLettersFound = uniqueWordsArray.filter(onlyWordWithLettersFound)
+
+  for (let i = 0; i < parseInt(wordsOnlyWithLettersFound.length); i++) {
     const newItem = document.createElement("li")
-    newItem.textContent = uniqueWordsArray[i].toUpperCase()
+    newItem.textContent = wordsOnlyWithLettersFound[i].toUpperCase()
     results.appendChild(newItem)
   }
-
 }
 
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
+}
+
+function onlyWordWithLettersFound(word) {
+  return lettersFound.every((letter, index) => {
+    if (letter === "?") {
+      return true
+    }
+    return letter === word.split("")[index]
+  })
 }
 
 function indexInClass(collection, node) {

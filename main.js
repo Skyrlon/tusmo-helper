3a3f4c0d769758e728d397bff09e6d1bcab50273
Antoine Drouault <antoinedrouault@hotmail.com>
2022-04-20 23:59:40 +0200

    Add comments for each functions

--- main.js
+++ main.js
@@ -9,10 +9,13 @@ lettersSubmitButton.addEventListener("click", handleLettersSubmit)
 
 let lettersFound = []
 
+//Create inputs as much there are letters in the word 
 function handleWordLengthSubmit() {
+  //Reset all data
   lettersFound = []
   letters.innerHTML = ""
   results.innerHTML = ""
+  //Create inputs container, inputs, and found toggle buttons
   for (let i = 0; i < parseInt(wordLengthInput.value); i++) {
     const letterContainer = `<div class="letter-container">
 <input class="letter-input" type="text" maxlength="1" size="1" />
@@ -22,16 +25,20 @@ function handleWordLengthSubmit() {
   }
   const letterButtons = document.getElementsByClassName("letter-button")
   const letterInputs = document.getElementsByClassName("letter-input")
+  //First is always already found
   letterInputs[0].classList.add("found")
+  //Add click events for found toggle buttons
   addEventListeners(letterButtons, "click", handleLetterButtonClick)
 }
 
+//Create event listeners
 function addEventListeners(elements, eventType, eventHandler) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, eventHandler)
   }
 }
 
+//Toggle letters is already found or not
 function handleLetterButtonClick(e) {
   e.preventDefault()
   const buttons = document.getElementsByClassName("letter-button")
@@ -39,15 +46,18 @@ function handleLetterButtonClick(e) {
   letterInputToModify.classList.toggle("found")
 }
 
+//Handle letters submitted
 function handleLettersSubmit(e) {
   handleLettersFound()
   getAllResults(e)
 }
 
+
 function handleLettersFound() {
   lettersFound = []
   const letterInputs = document.getElementsByClassName("letter-input")
   for (let i = 0; i < letterInputs.length; i++) {
+    //Check if there is a letter submitted or else replace it by "?"
     if (/^[a-zA-Z]+$/.test(letterInputs[i].value) && letterInputs[i].classList.contains("found")) {
       lettersFound.push(letterInputs[i].value)
     } else {
@@ -60,12 +70,15 @@ function getAllResults(e) {
   e.preventDefault()
   results.innerHTML = ""
   const lettersArray = []
+
+  //Fill lettersArray with letters submitted
   for (let i = 0; i < parseInt(wordLengthInput.value); i++) {
     lettersArray.push(document.getElementsByClassName("letter-input")[i].value)
   }
 
   const wordsArray = []
 
+  //Move each letters at time
   for (let i = 1; i < lettersArray.length; i++) {
     const letterToMove = lettersArray[i]
 
@@ -80,6 +93,7 @@ function getAllResults(e) {
 
   const wordsOnlyWithLettersFound = uniqueWordsArray.filter(onlyWordWithLettersFound)
 
+  //Show all results
   wordsOnlyWithLettersFound.forEach((word) => {
     let lettersHtml = []
     word.split("").forEach((letter, index) => {
@@ -94,14 +108,14 @@ function getAllResults(e) {
     newItem.innerHTML = lettersHtml.join("")
     results.appendChild(newItem)
   })
-
-
 }
 
+//Create array with unique values
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
+//Filter words with no letters already found
 function onlyWordWithLettersFound(word) {
   return lettersFound.every((letter, index) => {
     if (letter === "?") {
@@ -110,6 +124,7 @@ function onlyWordWithLettersFound(word) {
     return letter === word.split("")[index]
   })
 }
+
 
 function indexInClass(collection, node) {
   for (var i = 0; i < collection.length; i++) {
